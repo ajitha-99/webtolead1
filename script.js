a893113d2981225637304438cd2506f3e6bfec23
ajitha-99 <ajithag9999@gmail.com>
2025-07-17 19:04:12 -0500

    Adding callback and prevent the form to submit before recaptcha checkbox checked

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function beforesubmit() {
+function beforesubmit(event) {
+    if(captchaChecked){
    let outputdate =  document.querySelector(".outputdate");
    let inputdate = document.querySelector(".inputdate");
    console.log("inputdate.value", inputdate.value);//string -->date(en_US)
@@ -6,6 +7,11 @@ function beforesubmit() {
    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
    outputdate.value = formattedDate;
    console.log(outputdate.value);
+    }else{
+        alert('Please check the reCAPTCHA checkbox' );
+        event.preventDefault(); // it will prevent the submission of the form
+
+    }
 }
 
 function timestamp() { 
@@ -16,3 +22,7 @@ if (response == null || response.value.trim() == "") {
     document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); }
  } 
     setInterval(timestamp, 500); 
+
+    function captchasuccess(){
+        captchaChecked = true;
+    }
